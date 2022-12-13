44ad74f3adadadf2006d62be3bada5d3e1b8538f
Antonio Pontes <antonio.pontes@farmaciaindiana.com.br>
2022-12-13 17:00:33 -0300

    new: product's table using factory and seeder for fake data

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -3,10 +3,10 @@
 use App\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
